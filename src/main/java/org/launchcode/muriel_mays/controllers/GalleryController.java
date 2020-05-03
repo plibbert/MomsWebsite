b98952caad5494fad5bd1e79b51d1d6e6c73dae0
Your Name <you@example.com>
2020-05-03 15:36:18 -0500

    Updated Gallery to be slightly better.

--- src/main/java/org/launchcode/muriel_mays/controllers/GalleryController.java
+++ src/main/java/org/launchcode/muriel_mays/controllers/GalleryController.java
@@ -2,13 +2,15 @@ package org.launchcode.muriel_mays.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("gallery")
+@ResponseBody
 public class GalleryController {
 
-    @GetMapping("Gallery")
-    @ResponseBody
+    @GetMapping
     public String galleryPage(){
         return "This will be the gallery...eventually";
     }
