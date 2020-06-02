9b8ef0aead2b85d68c8096506800a7f1c3d76dd2
Your Name <you@example.com>
2020-06-02 18:24:36 -0500

    adding missing files

---
+++ src/test/java/org/launchcode/muriel_mays/MurielMaysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.muriel_mays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurielMaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
