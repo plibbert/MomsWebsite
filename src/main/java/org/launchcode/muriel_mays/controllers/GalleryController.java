2d4edc786e3cca571e988ba11fe903376cdfe57e
Your Name <you@example.com>
2020-06-07 00:14:12 -0500

    Added email function to order form, added more style/formatting throughout.

--- src/main/java/org/launchcode/muriel_mays/controllers/GalleryController.java
+++ src/main/java/org/launchcode/muriel_mays/controllers/GalleryController.java
@@ -1,20 +1,49 @@
 package org.launchcode.muriel_mays.controllers;
 
+import org.launchcode.muriel_mays.models.Recipe;
+import org.launchcode.muriel_mays.models.RecipeData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("gallery")
 public class GalleryController {
 
+    static HashMap<String, String> choices = new HashMap<>();
+
     @GetMapping
-    public String gallery(){
+    public String gallery() {
         return "gallery";
+    }
+
+//    public GalleryController() {
+//        choices.put("cookies", "Cookies, Bars, & Brownies");
+//        choices.put("cakes", "Cakes & Cupcakes");
+//        choices.put("breads", "Breads & Muffins");
+//        choices.put("pastry", "Pastry & Desserts");
+//        choices.put("savory", "Savory");
+//    }
+
+    @RequestMapping(value ="Images")
+    public String findImagesToDisplay(Model model, @RequestParam String category) {
+        ArrayList<String>images = new ArrayList<>();
+        String basePath = "src/main/resources/Static/images/" + category;
+            File i = new File(basePath);
+            for (String fileName : i.list()) {
+                images.add("/images/" + category + "/" + fileName);
+            }
+        model.addAttribute("title", category);
+            model.addAttribute("images", images);
+
+        return "/galleryDisplay";
     }
 }
--- src/main/java/org/launchcode/muriel_mays/controllers/RequestOrderController.java
+++ src/main/java/org/launchcode/muriel_mays/controllers/RequestOrderController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
+
 @Controller
 @RequestMapping("order")
 public class RequestOrderController {
@@ -15,10 +16,19 @@ public class RequestOrderController {
     }
 
     @PostMapping
-    public String submitOrder(@RequestParam String name, Model model){
-    model.addAttribute("formStatus","Your request has been sent. We will get in touch with you shortly!");
-        return "orders/form";
+    public String emailSender(@RequestParam String name, @RequestParam String emailAddress, @RequestParam String phoneNumber,
+                              @RequestParam String dateNeeded, @RequestParam String requestType, @RequestParam String description,
+                              @RequestParam String contact){
+        String subjectText = "Order Request" + requestType + dateNeeded;
+        String bodyText = "Name: " + name + "%0A" + "Email: " + emailAddress + "%0A" + "Phone Number: " + phoneNumber + "%0A" + "Date Needed: " + dateNeeded + "%0A" + "Request Type: " + requestType + "%0A" + "Description: " + description + "%0A" + "Preferred Contact Method: " + contact;
+        String mailtoText = "mailto:murielmays@gmail.com?subject=" + subjectText + "&body=" + bodyText;
+        return "redirect:" + mailtoText;
     }
 
+//    @PostMapping
+//    public String submitOrder(@RequestParam String name, Model model){
+//    model.addAttribute("formStatus","Your request has been sent. We will get in touch with you shortly!");
+//        return "orders/form";
+//    }
 
 }
